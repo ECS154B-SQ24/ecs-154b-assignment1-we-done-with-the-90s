e89e58c38d32eee051624fa088d5cde70f399089
Manny Hernandez <166497359+LeSunshine0@users.noreply.github.com>
2024-04-27 04:52:39 +0000

    Pass 5 test

--- src/main/scala/single-cycle/cpu.scala
+++ src/main/scala/single-cycle/cpu.scala
@@ -6,6 +6,14 @@ import chisel3._
 import chisel3.util._
 import dinocpu.components._
 
+class PCIncrementer extends Module {
+  val io = IO(new Bundle {
+    val pc = Input(UInt(64.W))
+    val newPC = Output(UInt(64.W))
+  })
+
+  io.newPC := io.pc + 4.U(64.W)
+}
 /**
  * The main CPU definition that hooks up all of the other components.
  *
@@ -22,11 +30,12 @@ class SingleCycleCPU(implicit val conf: CPUConfig) extends BaseCPU {
   val immGen          = Module(new ImmediateGenerator())
   val controlTransfer = Module(new ControlTransferUnit())
   val (cycleCount, _) = Counter(true.B, 1 << 30)
+  val incrementer = Module(new PCIncrementer())
 
-  control.io := DontCare
-  registers.io := DontCare
-  aluControl.io := DontCare
-  alu.io := DontCare
+  // control.io := DontCare
+  // registers.io := DontCare
+  // aluControl.io := DontCare
+  // alu.io := DontCare
   immGen.io := DontCare
   controlTransfer.io := DontCare
   io.dmem <> DontCare
@@ -43,7 +52,20 @@ class SingleCycleCPU(implicit val conf: CPUConfig) extends BaseCPU {
   }
 
   //Your code goes here
-
+  aluControl.io.aluop := control.io.aluop
+  aluControl.io.funct7 := instruction(31, 25)
+  aluControl.io.funct3 := instruction(14, 12)
+  control.io.opcode := instruction(6, 0)
+  alu.io.operation := aluControl.io.operation
+  alu.io.operand1 := registers.io.readdata1
+  alu.io.operand2 := registers.io.readdata2
+  registers.io.readreg1 := instruction(19, 15)
+  registers.io.readreg2 := instruction(24, 20)
+  registers.io.writereg := instruction(11, 7)
+  registers.io.writedata := alu.io.result
+  registers.io.wen := control.io.validinst & control.io.writeback_valid
+  incrementer.io.pc := pc
+  pc := incrementer.io.newPC
 }
 
 /*
